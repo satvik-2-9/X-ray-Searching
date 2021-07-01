f13626d61014e25db64f528d2e82ad4c142f3cf9
unknown <satvikbatra@gmail.com>
2021-07-01 14:57:23 +0530

    boolean recognition

--- src/Linkedin.js
+++ src/Linkedin.js
@@ -15,63 +15,46 @@ export default function Linkedin() {
     var link = "https://www.google.com/search?q=+";
   
   const generate = () => {
-    var quote = "\"";
-    
+    var quote = "\""; 
     if (jobTitle.length > 0) {
       var j = jobTitle.toUpperCase();
-      var tmp = "";
-      for (var i = 0; i < j.length; i++) {
-        if (j[i] === " ") {
-          tmp += '+';
-        } else {
-          tmp += j[i];
-          if (i === j.length - 1) {
-            link += (quote + tmp + quote);
-          }
+      const job = j.split(" ");
+      for (var i = 0; i < job.length; i++){
+        if (job[i].localeCompare("AND") === 0 || job[i].localeCompare("OR") === 0) { link += (job[i] + " ");} else {  
+          link += (quote + job[i] + quote + " "); 
         }
-      }
+      }   
     }
     
     if (location.length > 0) {
-      link += "+";
-      var tmp2 = "";
-      for (var i2 = 0; i2 < location.length; i2++) {
-        if (location[i2] === " ") {
-          link += (quote + tmp2 + quote);
-          link += " OR ";
-          tmp2 = "";
-          i2 += 3;
-          continue;
+      if (jobTitle.length > 0) { link += '+'; }
+      var loc = location.toUpperCase();
+      const l = loc.split(" "); 
+      for (var i2 = 0; i2 < l.length; i2++){
+        if (l[i2].localeCompare("AND") === 0 || l[i2].localeCompare("OR") === 0) {
+          link += (l[i2] + " "); 
         } else {
-          tmp2 += location[i2];
-          if (i2 === location.length - 1) {
-            link += (quote + tmp2 + quote);
-          }
+          link += (quote + l[i2] + quote + " "); 
         }
       }
     }
-    
     if (exclude.length > 0) {
       link += "-" + quote;
-      var tmp3 = "";
-      for (var i3 = 0; i3 < exclude.length; i3++) {
-        if (exclude[i3] === " ") {
-          link += (tmp3 + "+");
-          tmp3 = "";
-        } else {
-          tmp3 += exclude[i3];
-          if (i3 === exclude.length - 1) {
-            link += tmp3;
-          }
-        }
+      var tmp = exclude.toUpperCase();
+      var ex = tmp.split(" ");
+      for (var i3 = 0; i3 < ex.length; i3++){
+        link += (ex[i3] + " "); 
       }
-      link += quote;
+      link += quote; 
     }
+
     link += (" -intitle:" + quote + "profiles" + quote + " -inurl:" + quote + "dir/+" + quote + "+site:in.linkedin.com/in/+OR+site:in.linkedin.com/pub/");
+    
     if (education.length > 0) {
       var edu = education.toLowerCase();
       link += ("&as_oq=" + edu);
     }
+
     if (currentEmpoyer.length > 0) {
       link += ("+" + quote + "Current+%2A+" + currentEmpoyer + "%2A+" + quote);
     }
@@ -110,7 +93,7 @@ export default function Linkedin() {
     <div className="form-group row">
     <label  className="col-sm-2 col-form-label text-center">Keywords to exclude: </label>
     <div className="col-sm-10">
-            <input type="text" className="form-control text-center" id="inputPassword3" placeholder="Eg. Assistant OR Secretary" onChange={(e) => { setexclude(e.target.value) }}></input>
+            <input type="text" className="form-control text-center" id="inputPassword3" placeholder="Eg. Assistant AND Secretary" onChange={(e) => { setexclude(e.target.value) }}></input>
     </div>
       </div>
       
